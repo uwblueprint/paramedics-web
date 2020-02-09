ca1f136a895725af9d724dff1c56ac3ec9344872
Daniel <daniel.chen115@gmail.com>
2020-02-09 15:28:41 -0500

    Add user relation to event seeder

--- seeders/20200129021755-events.js
+++ seeders/20200129021755-events.js
@@ -1,31 +1,39 @@
 "use strict";
+
+const db = require('../models');
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
+    const user = await db.user.create(
+        {
+          firstName: 'Capt',
+          lastName: 'Holt',
+          email: 'capt.holt@asd.com',
+          password:'asdfgh1234',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+    );
+
     return queryInterface.bulkInsert("events", [
       {
         name: "St Patrick's Day",
         date: new Date(),
-        createdBy: {
-          firstName: "John",
-          lastName: "Doe",
-          email: "example@example.com",
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
+        createdBy: user.id,
         isActive: true,
         description: "bunch of drunk students",
         createdAt: new Date(),
         updatedAt: new Date()
+      },
+      {
+        name: "Homecoming",
+        date: new Date(),
+        createdBy: user.id,
+        isActive: true,
+        description: "a lot of drunk students",
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
-      // {
-      //   name: "Homecoming",
-      //   date: new Date(),
-      //   createdBy: 16,
-      //   isActive: true,
-      //   description: "a lot of drunk students",
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // }
     ]);
   },
   down: (queryInterface, Sequelize) => {
