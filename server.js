ec88a51153d4cf395b0fa636eb49632c9e395bc8
Daniel <daniel.chen115@gmail.com>
2020-02-26 21:19:15 -0500

    remove unnecessary flags

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const { ApolloServer } = require('apollo-server');
 const { schema } = require('./graphql');
 
-const server = new ApolloServer({ schema, playground: true, introspection: true });
+const server = new ApolloServer({ schema });
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
