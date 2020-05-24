ea3dc479873248ceb92d4e9a0c4a4b7a66ef5dea
Carelynn Tsai <carelynntsai@gmail.com>
2020-05-24 14:28:45 -0400

    add jest dependency and config

--- resolvers/__tests__/ambulance.test.js
+++ resolvers/__tests__/ambulance.test.js
@@ -1,8 +1,17 @@
-import { ambulanceResolvers } from '../ambulance.js';
-import ambulanceSchema from '../../schema/ambulance.js';
+describe('Test ambulance resolvers', () => {
+    test('Get ambulances', () => {
+        expect('this').toEqual('this')
+    })
 
+    test('Add ambulance', () => {
+        expect('this').toEqual('this')
+    })
 
+    test('Delete ambulance', () => {
+        expect('this').toEqual('this')
+    })
 
-test('sample', () => {
-    expect('this').toEqual('this')
+    test('Update ambulance', () => {
+        expect('this').toEqual('this')
+    })
 })
