27e76795b94a83be571ca923f55f44ea25167ab4
Carelynn Tsai <carelynntsai@gmail.com>
2020-05-25 20:42:46 -0400

    unit testing Co-authored-by: Aaron Abraham <aaronabraham311@gmail.com>

--- resolvers/__tests__/ambulance.test.js
+++ resolvers/__tests__/ambulance.test.js
@@ -1,10 +1,81 @@
-describe('Test ambulance resolvers', () => {
-    test('Get ambulances', () => {
-        expect('this').toEqual('this')
-    })
+const { ApolloServer } = require('apollo-server');
+const { graphql } = require('graphql');
+const { casual } = require('casual');
+const { createTestClient } = require('apollo-server-testing')
+const { createApolloFetch } = require('apollo-fetch');
+const { schema } = require('../../graphql');
 
-    test('Add ambulance', () => {
-        expect('this').toEqual('this')
+const ambulance_mocks = {
+    ambulance: () => ({
+        vehicleNumber:  casual.integer(0, 10),
+        createdAt: casual.date(format = 'YYYY-MM-DD'),
+        updatedAt: casual.date(format = 'YYYY-MM-DD')
+    })
+};
+
+const fetch = createApolloFetch({
+    uri: 'http://localhost:4000/graphql',
+});
+  
+const apolloServer = new ApolloServer({
+    schema,
+    mocks: ambulance_mocks,
+    mockEntireSchema: false
+});
+
+describe('Test ambulance resolvers', () => {
+    // const { query } = createTestClient(apolloServer);
+    // const res = await query({ query: ambulances });
+    // expect('this').toEqual('this');
+    
+    // let url;
+    // let httpServer;
+
+    // beforeEach(async () => {
+    //     ; ({ url, server: httpServer } = await apolloServer.listen());
+    //     console.log(`🚀 Server ready at ${url}`);
+    // });
+
+    // afterEach(() => {
+    //     httpServer.close();
+    // });
+
+    // test('Get ambulances', async () => {
+    //     const example_query = `
+    //         {
+    //             ambulances {
+    //                 id 
+    //                 vehicleNumber
+    //                 createdAt
+    //                 updatedAt
+    //             }
+    //         }
+    //     `
+
+    //     const res = await fetch({
+    //         query: example_query,
+    //       }).then(res => {
+    //         console.log(res.data);
+    //       }).catch((error) => console.log(error));
+    //     expect('this').toEqual('this')
+    // })
+
+    test('Add ambulance', async () => {
+        const example_query = `
+            {
+                ambulances {
+                    id 
+                    vehicleNumber
+                    createdAt
+                    updatedAt
+                }
+            }
+        `
+        
+        const { query } = createTestClient(apolloServer);
+        const res = await query({ query: example_query });
+        console.log(res);
+        expect('this').toEqual('this');
     })
 
     test('Delete ambulance', () => {
