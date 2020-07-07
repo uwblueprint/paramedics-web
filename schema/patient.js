5d8ca237c1a35de82f8949dba4b7b45a11997937
Aaron Abraham <aaronabraham311@gmail.com>
2020-07-07 14:59:18 -0600

    Added DELETED enum to schema

--- schema/patient.js
+++ schema/patient.js
@@ -11,6 +11,7 @@ const patientSchema = `
     ON_SITE
     RELEASED
     TRANSPORTED
+    DELETED
   }
 
   extend type Query {
@@ -64,7 +65,5 @@ const patientSchema = `
     createdAt: DateTime,
     updatedAt: DateTime,
   }
-  `
-  ;
-
+  `;
 exports.patientSchema = patientSchema;
