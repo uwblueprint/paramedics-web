9e14e73d043b05c57cc2d8ecb6b815be6e7179ff
Aaron Abraham <aaronabraham311@gmail.com>
2020-07-14 19:51:08 -0600

    Migration for locationPins table

---
+++ migrations/20200715014355-create-location-pins.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("locationPins", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      label: {
+        type: Sequelize.STRING,
+      },
+      latitude: {
+        type: Sequelize.DOUBLE,
+      },
+      longitude: {
+        type: Sequelize.DOUBLE,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      eventId: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("locationPins");
+  },
+};
