617ae1d6f26d60368ef5026d4d8d7d76297823cc
Carelynn Tsai <carelynntsai@gmail.com>
2020-07-19 23:07:46 -0400

    fixed migration

--- migrations/20200719025533-add-name-column-users.js
+++ migrations/20200719025533-add-name-column-users.js
@@ -2,51 +2,63 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction(async (t) => {
-      await queryInterface.addColumn(
-        "users",
-        "name",
-        {
-          type: Sequelize.DataTypes.STRING,
-        },
-        { transaction: t }
-      )
-      await queryInterface.sequelize.query(
-        `UPDATE users SET "name" = "firstName" WHERE "lastName"=''`,
-        { transaction: t }
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE users SET "name" = CONCAT("firstName", ' ', "lastName") WHERE "lastName"<>''`,
-        { transaction: t }
-      );
-      await queryInterface.removeColumn('users', 'firstName', { transaction: t });
-      return queryInterface.removeColumn('users', 'lastName', { transaction: t });
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.addColumn(
+          "users",
+          "name",
+          {
+            type: Sequelize.DataTypes.STRING,
+          },
+          { transaction: t }
+        ),
+        queryInterface.sequelize.query(
+          `UPDATE users SET "name" = "firstName" WHERE "lastName"='' AND "firstName"<>''`,
+          { transaction: t }
+        ),
+        queryInterface.sequelize.query(
+          `UPDATE users SET "name" = "lastName" WHERE "firstName"='' AND "lastName"<>''`,
+          { transaction: t }
+        ),
+        queryInterface.sequelize.query(
+          `UPDATE users SET "name" = CONCAT("firstName", ' ', "lastName") WHERE "lastName"<>'' AND "firstName"<>''`,
+          { transaction: t }
+        ),
+        queryInterface.removeColumn('users', 'firstName', { transaction: t }),
+        queryInterface.removeColumn('users', 'lastName', { transaction: t }),
+      ]);
     });
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction(async (t) => {
-      await queryInterface.addColumn(
-        "users",
-        "firstName",
-        {
-          type: Sequelize.DataTypes.STRING,
-        },
-        { transaction: t }
-      );
-      await queryInterface.addColumn(
-        "users",
-        "lastName",
-        {
-          type: Sequelize.DataTypes.STRING,
-        },
-        { transaction: t }
-      );
-      await queryInterface.sequelize.query(
-        'UPDATE users SET "firstName" = "name", "lastName" = "name"',
-        { transaction: t }
-      );
-      return queryInterface.removeColumn('users', 'name', { transaction: t });
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.addColumn(
+          "users",
+          "firstName",
+          {
+            type: Sequelize.DataTypes.STRING,
+          },
+          { transaction: t }
+        ),
+        queryInterface.addColumn(
+          "users",
+          "lastName",
+          {
+            type: Sequelize.DataTypes.STRING,
+          },
+          { transaction: t }
+        ),
+        queryInterface.sequelize.query(
+          `UPDATE users SET "firstName" = "name", "lastName" = '' WHERE "name" NOT LIKE '% %'`,
+          { transaction: t }
+        ),
+        queryInterface.sequelize.query(
+          `UPDATE "users" SET "firstName" = SPLIT_PART("name", ' ', 1), "lastName" = SPLIT_PART("name", ' ', 2) WHERE "name" LIKE '% %'`,
+          { transaction: t }
+        ),
+        queryInterface.removeColumn('users', 'name', { transaction: t }),
+      ]);
     });
   }
 };
