dc08b3668beb88aa0f1443799edb2785e075dde2
Jason Guo <jason.guo1@uwaterloo.ca>
2020-07-24 22:07:48 -0400

    fix down migration for barcodeValue

--- migrations/20200719172434-change-patients-barcodeValue-to-string.js
+++ migrations/20200719172434-change-patients-barcodeValue-to-string.js
@@ -1,24 +1,19 @@
 "use strict";
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all([
-        queryInterface.changeColumn(
-          "patients",
-          "barcodeValue",
-          { type: Sequelize.DataTypes.STRING },
-          {
-            transaction: t,
-          }
-        ),
-      ]);
-    });
+     up: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn(
+      "patients", "barcodeValue", { type: Sequelize.DataTypes.STRING }
+    );
   },
-
   down: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction((t) => {
       return Promise.all([
+        queryInterface.bulkUpdate('patients',
+          { barcodeValue: null },
+          { barcodeValue: { [Sequelize.Op.regexp]: '[^0-9]' } },
+          { transaction: t }
+        ),
         queryInterface.changeColumn(
           "patients",
           "barcodeValue",
