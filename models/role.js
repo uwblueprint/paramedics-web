ee24e0fa726c47fdac01b514a3566e305715e422
Ricky Mao <rickyrmao@gmail.com>
2020-09-23 11:16:46 -0700

    changed to plural role
    Co-authored-by: petorsong <59516223+petorsong@users.noreply.github.com>

--- models/role.js
+++ models/role.js
@@ -2,7 +2,7 @@
 
 module.exports = (sequelize, DataTypes) => {
   const role = sequelize.define(
-    'role',
+    'roles',
     {
       name: DataTypes.STRING,
       displayName: DataTypes.STRING,
