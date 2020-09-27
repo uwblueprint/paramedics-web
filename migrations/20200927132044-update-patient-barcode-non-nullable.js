6e6173b1ae4d2fc35f724b0a3595226271a1ae84
Patrick Du <patrickdu@uwblueprint.org>
2020-09-27 18:37:08 -0400

    Use Async/Await and replace raw SQL with sequelize method

--- migrations/20200927132044-update-patient-barcode-non-nullable.js
+++ migrations/20200927132044-update-patient-barcode-non-nullable.js
@@ -1,41 +1,24 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface) => {
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all([
-        queryInterface.sequelize.query(
-          `UPDATE patients SET "barcodeValue" = "id" WHERE "barcodeValue" IS NULL`,
-          { transaction: t }
-        ),
-        queryInterface.sequelize.query(
-          `ALTER TABLE patients ALTER COLUMN "barcodeValue" TYPE "varchar", ALTER COLUMN "barcodeValue" SET NOT NULL`,
-          { transaction: t }
-        ),
-        queryInterface.sequelize.query(
-          `ALTER TABLE patients ADD CONSTRAINT unique_barcode UNIQUE ("barcodeValue")`,
-          { transaction: t }
-        ),
-      ]);
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(
+      `UPDATE patients SET "barcodeValue" = "id" WHERE "barcodeValue" IS NULL`
+    );
+    return queryInterface.changeColumn('patients', 'barcodeValue', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
     });
   },
-
-  down: (queryInterface) => {
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all([
-        queryInterface.sequelize.query(
-          `ALTER TABLE patients DROP CONSTRAINT "unique_barcode"`,
-          { transaction: t }
-        ),
-        queryInterface.sequelize.query(
-          'ALTER TABLE patients ALTER COLUMN "barcodeValue" TYPE "varchar", ALTER COLUMN "barcodeValue" DROP NOT NULL',
-          { transaction: t }
-        ),
-        queryInterface.sequelize.query(
-          `UPDATE patients SET "barcodeValue" = NULL WHERE "barcodeValue" = "id"::varchar`,
-          { transaction: t }
-        ),
-      ]);
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('patients', 'barcodeValue', {
+      type: Sequelize.STRING,
+      allowNull: true,
+      unique: false,
     });
+    return queryInterface.sequelize.query(
+      `UPDATE patients SET "barcodeValue" = NULL WHERE "barcodeValue" = "id"::varchar`
+    );
   },
 };
