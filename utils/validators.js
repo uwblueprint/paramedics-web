a52ca44917b0a96b56fddcd14dca636f4cfbe906
Ricky Mao <rickyrmao@gmail.com>
2020-09-30 01:04:34 +0000

    added validateRole

--- utils/validators.js
+++ utils/validators.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { AuthenticationError } = require('apollo');
 const db = require('../models');
 
 // for validating resolver arguments
@@ -40,4 +41,43 @@ module.exports = {
         throw new Error(errorMessage);
       }
     }),
+  validatePatient: (
+    patientId,
+    checkParanoid = false,
+    errorMessage = 'Invalid patient ID: ' + patientId
+  ) => {
+    const options = { paranoid: true };
+    if (checkParanoid) {
+      options.paranoid = false;
+    }
+    return db.patient.findByPk(patientId, options).then((patient) => {
+      if (!patient) {
+        throw new Error(errorMessage);
+      }
+    });
+  },
+  validateLocationPin: (
+    locationPinId,
+    checkParanoid = false,
+    errorMessage = 'Invalid location pin ID: ' + locationPinId
+  ) => {
+    const options = { paranoid: true };
+    if (checkParanoid) {
+      options.paranoid = false;
+    }
+    return db.locationPins
+      .findByPk(locationPinId, options)
+      .then((locationPin) => {
+        if (!locationPin) {
+          throw new Error(errorMessage);
+        }
+      });
+  },
+  validateRole: (role, errorMessage = 'Insufficient permission') => {
+    // TODO: remove user role when authentication is done
+    const userRole = 'Commander';
+    if (role !== userRole) {
+      throw new AuthenticationError(errorMessage);
+    }
+  },
 };
