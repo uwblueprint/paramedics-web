3430ecfc42345126b83f6bc4ffe34655a97c9d22
Sara Fong <sara.fong@uwaterloo.ca>
2020-10-17 15:09:50 -0400

    more changes'

--- migrations/20201015005801-update-user-roles.js
+++ migrations/20201015005801-update-user-roles.js
@@ -6,65 +6,22 @@ module.exports = {
       return Promise.all([
         queryInterface.addColumn(
           'users',
-          'role',
+          'roleId',
           { type: Sequelize.DataTypes.INTEGER },
           { transaction: t }
         ),
         queryInterface.sequelize.query(
-          `UPDATE users SET "role" = 1 WHERE "accessLevel" = 'COMMANDER' OR "accessLevel" = 'ADMIN'`,
+          `UPDATE users SET "roleId" = 1 WHERE "accessLevel" = 'COMMANDER' OR "accessLevel" = 'ADMIN'`,
           { transaction: t }
         ),
-        queryInterface.sequelize.query(
-          `UPDATE users SET "role" = 2 WHERE "accessLevel" = 'SUPERVISOR'`,
+        queryInterface.sequelize.query( 
+          `UPDATE users SET "roleId" = 2 WHERE "accessLevel" = 'SUPERVISOR'`,
           { transaction: t }
         ),
-        // queryInterface.sequelize.query(
-        //   `UPDATE users SET "role" = 3 WHERE "accessLevel" = 'DISPATCH'`
-        // ,
-        // { transaction: t }
-        // ),
         queryInterface.removeColumn('users', 'accessLevel', {
           transaction: t,
-        }),
+        })
       ]);
-      //   return Promise(() => {
-      //     queryInterface.addColumn(
-      //       'users',
-      //       'role',
-      //       { type: Sequelize.DataTypes.INTEGER },
-      //       { transaction: t }
-      //     )
-      //   })
-      //   .then(() => {
-      //     return Promise(() => {
-      //       queryInterface.sequelize.query(
-      //         `UPDATE users SET "role" = 1 where "accessLevel" = "COMMANDER"`,
-      //       { transaction: t }
-      //       )})
-      //       .then(() => {
-      //       return Promise(() => {
-      //         queryInterface.sequelize.query(
-      //           `UPDATE users SET "role" = 2 where "accessLevel" = "SUPERVISOR"`
-      //         ,
-      //         { transaction: t }
-      //       )})
-      //         .then(() => {
-      //           return Promise(() => {
-      //             queryInterface.sequelize.query(
-      //               `UPDATE users SET "role" = 3 where "accessLevel" = "DISPATCH"`
-      //             ,
-      //             { transaction: t }
-      //           )})
-      //           .then(() => {
-      //             return Promise(() => {
-      //               queryInterface.removeColumn('users', 'accessLevel', {
-      //                 transaction: t,
-      //               })
-      //               });
-      //           });
-      //         });
-      //       });
-      //   });
     });
   },
 
@@ -80,62 +37,22 @@ module.exports = {
           { transaction: t }
         ),
         queryInterface.sequelize.query(
-          `UPDATE users SET "accessLevel" = 'COMMANDER' WHERE "role" = 1`,
+          `UPDATE users SET "accessLevel" = 'COMMANDER' WHERE "roleId" = 1`,
           { transaction: t }
         ),
         queryInterface.sequelize.query(
-          `UPDATE users SET "accessLevel" = 'SUPERVISOR' WHERE "role" = 2`,
+          `UPDATE users SET "accessLevel" = 'SUPERVISOR' WHERE "roleId" = 2`,
           { transaction: t }
         ),
         queryInterface.sequelize.query(
-          `UPDATE users SET "accessLevel" = 'DISPATCH' WHERE "role" = 3`,
+          `UPDATE users SET "accessLevel" = 'DISPATCH' WHERE "roleId" = 3`,
           { transaction: t }
         ),
-        queryInterface.removeColumn('users', 'role', {
+        queryInterface.removeColumn('users', 'roleId', {
           transaction: t,
         }),
       ]);
     });
-    // return queryInterface.sequelize.transaction((t) => {
-    //   return Promise(() => {
-    //     queryInterface.addColumn(
-    //       'users',
-    //       'accessLevel',
-    //       { type: Sequelize.DataTypes.ENUM('COMMANDER', 'SUPERVISOR', 'ADMIN') },
-    //       { transaction: t }
-    //     )
-    //   })
-    //   .then(() => {
-    //     return Promise(() => {
-    //       queryInterface.sequelize.query(
-    //         `UPDATE users SET "accessLevel" = "COMMANDER" where "role" = 1`
-    //       ,
-    //       { transaction: t }
-    //     )})
-    //       .then(() => {
-    //       return Promise(() => {
-    //         queryInterface.sequelize.query(
-    //           `UPDATE users SET "accessLevel" = "SUPERVISOR" where "role" = 2`
-    //         ,
-    //         { transaction: t }
-    //       )})
-    //         .then(() =>{
-    //           return Promise(() => {
-    //             queryInterface.sequelize.query(
-    //               `UPDATE users SET "accessLevel" = "DISPATCH" where "role" = 3`
-    //             ,
-    //             { transaction: t }
-    //           )})
-    //           .then(() => {
-    //             return Promise(() => {
-    //               queryInterface.removeColumn('users', 'accessLevel', {
-    //                 transaction: t,
-    //               })
-    //               });
-    //           });
-    //         });
-    //       });
-    //   });
-    // });
+  
   },
 };
