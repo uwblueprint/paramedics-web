9ed1b8e5925520110cd81de14e6bd7689a14710a
Sara Fong <sara.fong@uwaterloo.ca>
2020-10-27 21:33:00 -0400

    fixed PR issues

--- migrations/20201015005801-update-user-roles.js
+++ migrations/20201015005801-update-user-roles.js
@@ -1,57 +1,57 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all([
-        queryInterface.addColumn(
-          'users',
-          'roleId',
-          { type: Sequelize.DataTypes.INTEGER },
-          { transaction: t }
-        ),
-        queryInterface.sequelize.query(
-          `UPDATE users SET "roleId" = 1 WHERE "accessLevel" = 'COMMANDER' OR "accessLevel" = 'ADMIN'`,
-          { transaction: t }
-        ),
-        queryInterface.sequelize.query(
-          `UPDATE users SET "roleId" = 2 WHERE "accessLevel" = 'SUPERVISOR'`,
-          { transaction: t }
-        ),
-        queryInterface.removeColumn('users', 'accessLevel', {
-          transaction: t,
-        }),
-      ]);
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn(
+        'users',
+        'roleId',
+        { type: Sequelize.DataTypes.INTEGER },
+        { transaction: t }
+      );
+      await queryInterface.sequelize.query(
+        `UPDATE users SET "roleId" = 1 WHERE "accessLevel" = 'COMMANDER' OR "accessLevel" = 'ADMIN'`,
+        { transaction: t }
+      );
+      await queryInterface.sequelize.query(
+        `UPDATE users SET "roleId" = 2 WHERE "accessLevel" = 'SUPERVISOR'`,
+        { transaction: t }
+      );
+      await queryInterface.removeColumn('users', 'accessLevel', {
+        transaction: t,
+      });
+      await queryInterface.sequelize.query(
+        'DROP TYPE IF EXISTS "enum_users_accessLevel";',
+        { transaction: t }
+      );
     });
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all([
-        queryInterface.addColumn(
-          'users',
-          'accessLevel',
-          {
-            type: Sequelize.DataTypes.STRING,
-          },
-          { transaction: t }
-        ),
-        queryInterface.sequelize.query(
-          `UPDATE users SET "accessLevel" = 'COMMANDER' WHERE "roleId" = 1`,
-          { transaction: t }
-        ),
-        queryInterface.sequelize.query(
-          `UPDATE users SET "accessLevel" = 'SUPERVISOR' WHERE "roleId" = 2`,
-          { transaction: t }
-        ),
-        queryInterface.sequelize.query(
-          `UPDATE users SET "accessLevel" = 'DISPATCH' WHERE "roleId" = 3`,
-          { transaction: t }
-        ),
-        queryInterface.removeColumn('users', 'roleId', {
-          transaction: t,
-        }),
-      ]);
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn(
+        'users',
+        'accessLevel',
+        {
+          type: Sequelize.DataTypes.ENUM('COMMANDER', 'SUPERVISOR', 'ADMIN'),
+        },
+        { transaction: t }
+      );
+      await queryInterface.sequelize.query(
+        `UPDATE users SET "accessLevel" = 'COMMANDER' WHERE "roleId" = 1`,
+        { transaction: t }
+      );
+      await queryInterface.sequelize.query(
+        `UPDATE users SET "accessLevel" = 'SUPERVISOR' WHERE "roleId" = 2`,
+        { transaction: t }
+      );
+      await queryInterface.sequelize.query(
+        `UPDATE users SET "accessLevel" = 'DISPATCH' WHERE "roleId" = 3`,
+        { transaction: t }
+      );
+      await queryInterface.removeColumn('users', 'roleId', {
+        transaction: t,
+      });
     });
   },
 };
