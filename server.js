912b39acf9868a8ba0c900e8ce3d39b8ec820e45
Jason Guo <jason.guo1@uwaterloo.ca>
2020-11-03 19:51:15 -0500

    add logout functionality

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
@@ -14,33 +16,44 @@ const PORT = 4000;
 const HOST = `http://localhost:${PORT}`;
 const SESSION_SECRET = 'notsecure';
 
-passport.use(
-  new saml.Strategy(
-    {
-      path: '/login/callback',
-      entryPoint:
-        'https://uwbp-paramedics.us.auth0.com/samlp/S5q8F6MRwUoeg8VzC9gmkCVjTpNkkZXz',
+const samlStrategy = new saml.Strategy(
+  {
+    path: '/login/callback',
+    entryPoint:
+      `https://uwbp-paramedics.us.auth0.com/samlp/${process.env.AUTH0_CLIENT_ID}`,
+    acceptedClockSkewMs: -1, // SAML assertion not yet valid fix
+    logoutUrl: 'https://uwbp-paramedics.us.auth0.com/v2/logout',
+    additionalLogoutParams: {
+      // TODO: Move this to env variable
+      client_id: process.env.AUTH0_CLIENT_ID,
+      federated: '',
+      returnTo: 'http://localhost:4000/logout/callback',
     },
-    (profile, done) => {
-      const email =
-        profile[
-          'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'
-        ];
-      db.user
-        .findOne({
-          where: { email: { [Op.like]: email } },
-        })
-        .then((matchingUser) =>
-          done(
-            matchingUser
-              ? null
-              : new Error('Email is not registered: ' + email),
-            matchingUser
-          )
+  },
+  (profile, done) => {
+    // TODO: This will need to be changed when we switch to ADFS
+    const email =
+      profile[
+        'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'
+      ];
+    db.user
+      .findOne({
+        where: { email: { [Op.like]: email } },
+      })
+      .then((matchingUser) => {
+        // Save the nameId and nameIDFormat for logout
+        matchingUser.saml = {};
+        matchingUser.saml.nameID = profile.nameID;
+        matchingUser.saml.nameIDFormat = profile.nameIDFormat;
+        done(
+          matchingUser ? null : new Error('Email is not registered: ' + email),
+          matchingUser
         );
-    }
-  )
+      });
+  }
 );
+
+passport.use(samlStrategy);
 
 passport.serializeUser((user, done) => {
   done(null, user.email);
@@ -62,7 +75,8 @@ app.use(
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 1000 },
+    // TODO: Handle login cookie expiring
+    // cookie: { maxAge: 1000 },
   })
 );
 
@@ -74,19 +88,30 @@ app.use(bodyParser.json());
 
 app.get('/login', passport.authenticate('saml'));
 
+// TODO: Redirect to frontend auth landing page
 app.post('/login/callback', passport.authenticate('saml'), (req, res) => {
-  console.log('-----------------------------');
-  console.log('login call back dumps');
-  console.log(req.session);
-  console.log(req.user);
-  console.log('-----------------------------');
-  res.send('Log in Callback Success');
+  if (!req.user) {
+    res.send("Login unsuccessful")
+  } else {
+    res.send("Login successful!")
+  }
 });
 
 app.get('/logout', (req, res) => {
-  req.logout();
-  res.clearCookie('connect.sid');
-  res.send('Logged out');
+  if (req.user == null) {
+    res.send('No logged in user!');
+    return;
+  }
+  samlStrategy.logout(req, function (err, uri) {
+    // Clear user login session on our end
+    res.clearCookie('connect.sid');
+    // Redirect to logout callback URL
+    return res.redirect(uri);
+  });
+});
+// TODO: Redirect to frontend login screen
+app.get('/logout/callback', (req, res) => {
+  res.send('Logout callback');
 });
 
 // ** setup apollo server + middleware + run
@@ -103,6 +128,6 @@ const server = new ApolloServer({
   },
 });
 
-server.applyMiddleware({ app /* , cors: false */ });
+server.applyMiddleware({ app });
 
 app.listen({ port: PORT }, () => console.log(`🚀 Server ready at ${HOST}`));
